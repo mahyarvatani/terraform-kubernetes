3c60072c076165e0dd7b26f5696b90b91908afeb
Vatanizadeh Mahyar <M_Vatanizadeh@iranet.net>
2023-02-08 10:13:17 +0330

    seperat provider

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.17.0"
-    }
-  }
-}
 
-provider "kubernetes" {
-  config_path    = "C:\\Users\\m_vatanizadeh\\.kube\\config"
-}
-
-resource "kubernetes_namespace" "mahyar" {
+resource "kubernetes_namespace" "fms" {
   metadata {
-    name = "mahyar"
+    name = "fms
+    "
   }
 }
+
